87ceb23f9ead82f39fd319ef41cbfb82661d439f
alirafaqat <alirafaqat@hotmail.com>
2017-09-15 10:28:14 +1000

    Added error response

---
+++ userstring/src/main/java/com/test/controllers/CustomErrorController.java
@@ -0,0 +1,24 @@
+package com.test.controllers;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+	private static final String PATH = "/error";
+	
+	@RequestMapping(value = PATH)
+	@ResponseBody
+    public ResponseEntity<String> error() {
+		return new ResponseEntity("NOT_FOUND", HttpStatus.NOT_FOUND);
+    }
+	
+	@Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
